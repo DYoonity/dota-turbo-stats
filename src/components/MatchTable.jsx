7b16848dfae8e970b6ca9d2446fc15c6448a9021
David Yoon <DYoonity@users.noreply.github.com>
2023-08-30 17:25:00 -0400

    match table ui component

--- src/components/MatchTable.jsx
+++ src/components/MatchTable.jsx
@@ -1,21 +1,51 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getMatches } from '../features/matches/matchesSlice';
+import React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+import {
+  useGetHeroMatchesQuery,
+  useGetMatchQuery,
+} from '../features/matches/openDotaApiSlice';
 
 function MatchTable() {
-  const { matchesList, loading } = useSelector(state => state.matches);
-  const dispatch = useDispatch();
+  const {
+    data: heroMatchesData,
+    error,
+    isSuccess,
+    isError,
+    isLoading,
+  } = useGetHeroMatchesQuery(15);
 
-  // useEffect(() => {
-  //   dispatch(getMatches());
-  // }, []);
+  let heroMatchesContent;
 
-  if (loading) return <p>Loading...</p>;
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (isSuccess) {
+    heroMatchesContent;
+  } else if (isError) {
+    heroMatchesContent = <p>{error}</p>;
+  }
+
+  const columns = [
+    { field: 'matchId', headerName: 'Match ID', width: 150 },
+    { field: 'hero', headerName: 'Hero', width: 150 },
+    { field: 'kills', headerName: 'Kills', width: 150 },
+    { field: 'deaths', headerName: 'Deaths', width: 150 },
+    { field: 'itemTimings', headerName: 'Item Timings', width: 150 },
+  ];
+
+  const dataRows = heroMatchesData.map((row, index) => ({
+    id: index + 1,
+    matchId: row.match_id,
+  }));
 
   return (
     <div className='matchTable'>
-      <h2>DOTA Turbo Matches</h2>
-      <button onClick={() => dispatch(getMatches(15))}> Get matches</button>
+      <Box sx={{ height: 520, width: '100%' }}>
+        <h2>DOTA Matches - Hero - Item Timings</h2>
+        {/* <button onClick={() => dispatch(getMatches(15))}> Get matches</button> */}
+        <DataGrid rows={dataRows} columns={columns} />
+      </Box>
     </div>
   );
 }
